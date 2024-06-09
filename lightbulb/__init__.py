aec4f3d933dd57a271ff909c08199dfe69db2d35
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2024-06-09 10:03:17 -0700

    Adding revised logging which uses custom request/response logging formatting with colors and remove unneeded logging from views

--- lightbulb/__init__.py
+++ lightbulb/__init__.py
@@ -1,54 +1,122 @@
 import os
-from flask import Flask
-from logging import getLogger
+from flask import Flask, request, g
 
-from lightbulb.logging import handler as lightbulb_handler
-from lightbulb.factory import Lightbulb
+import time
+import datetime
+from rfc3339 import rfc3339
+import colors
+import logging
+
 from lightbulb.main.views import main_blueprint
+
+class CustomRequestFormatter(logging.Formatter):
+    def format(self, record):
+        return super().format(record)
+    
+
+system_handler = logging.StreamHandler()
+system_handler.setFormatter(CustomRequestFormatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+))
+
+lightbulb_logger = logging.getLogger('app')
+lightbulb_logger.setLevel(logging.DEBUG)
+
+lightbulb_handler = logging.StreamHandler()
+lightbulb_handler.setFormatter(logging.Formatter(''))
+
+lightbulb_logger.addHandler(lightbulb_handler)
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    lightbulb = Lightbulb()
+    app.logger.addHandler(system_handler)
 
-    lightbulb.init_app(app)
-    
-    if app.logger.handlers:
+    if len(app.logger.handlers) > 1:
         app.logger.removeHandler(app.logger.handlers[0])
 
+
+    werkzueg_logger = logging.getLogger('werkzeug')
+    werkzueg_logger.setLevel('ERROR')
+
     if os.environ.get('FLASK_ENV') == 'development':
-        app.logger.info('Creating app in development mode')
         app.logger.setLevel('DEBUG')
         app.debug = True
     else:
-        app.logger.info('Creating app in production mode')
         app.logger.setLevel('INFO')
         app.debug = False
-    
 
-    app.logger.info(f"App: {__name__} | VIRTUAL_ENV = {os.environ.get('VIRTUAL_ENV', str(None))}")
-    app.logger.info(f"App: {__name__} | FLASK_DEBUG = {app.debug}")
-    app.logger.info(f"App: {__name__} | FLASK_RUN_FROM_CLI = {os.environ.get('FLASK_RUN_FROM_CLI')}")
-    app.logger.info(f"App: {__name__} | FLASK_ENV = {os.environ.get('FLASK_ENV')}")
-    app.logger.info(f"App: {__name__} | FLASK_APP = {os.environ.get('FLASK_APP')}")
+    lightbulb_logger.info(f"VIRTUAL_ENV = {os.environ.get('VIRTUAL_ENV', str(None))}")
+    lightbulb_logger.info(f"FLASK_DEBUG = {app.debug}")
+    lightbulb_logger.info(f"FLASK_RUN_FROM_CLI = {os.environ.get('FLASK_RUN_FROM_CLI')}")
+    lightbulb_logger.info(f"FLASK_ENV = {os.environ.get('FLASK_ENV')}")
+    lightbulb_logger.info(f"FLASK_APP = {os.environ.get('FLASK_APP')}")
 
-    app.logger.debug("Environment Variables | " + str(os.environ))
-
-    app.logger.info("Setting app.config values")
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('LIGHTBULB_SECRET_KEY', '0000000000000000000000000000000000'),
     )
-    app.logger.debug(f"'app.config['SECRET_KEY'] = [LIGHTBULB_SECRET_KEY: {app.config['SECRET_KEY']}]")
 
-    app.logger.info("Registering blueprints")
+    lightbulb_logger.info(f"LIGHTBULB_SECRET_KEY: {app.config['SECRET_KEY']}")
 
+    blueprint_start = time.time()
     app.register_blueprint(main_blueprint)
-    app.logger.debug("Registered main_blueprint")
+    blueprint_end = time.time()
+
+    blueprint_duration = round(blueprint_end - blueprint_start, 2)
+
+    app.logger.debug(f"blueprints loaded in {blueprint_duration} seconds.")
+    
+    @app.before_request
+    def start_timer():
+        g.start = time.time()
+
+    # Log request/response info for each request after the request is processed
+    @app.after_request
+    def log_request(response):
+        if request.path == '/favicon.ico':
+            return response
+        elif request.path.startswith('/static'):
+            return response
+
+        now = time.time()
+        duration = round(now - g.start, 2)
+        dt = datetime.datetime.fromtimestamp(now)
+        timestamp = rfc3339(dt, utc=True)
+
+        ip = request.headers.get('X-Real-IP', request.remote_addr)
+        host = request.host.split(':', 1)[0]
+        args = dict(request.args)
+
+        log_params = [
+            ('method', request.method, 'blue'),
+            ('path', request.path, 'blue'),
+            ('status', response.status_code, 'yellow'),
+            ('duration', duration, 'green'),
+            ('time', timestamp, 'magenta'),
+            ('ip', ip, 'red'),
+            ('host', host, 'red'),
+            ('params', args, 'blue')
+        ]
+
+        request_id = request.headers.get('X-Request-ID')
+        if request_id:
+            log_params.append(('request_id', request_id, 'yellow'))
+
+        parts = []
+
+        for name, value, color in log_params:
+            part = colors.color("{}={}".format(name, value), fg=color)
+            parts.append(part)
+            line = " ".join(parts)
+
+        lightbulb_logger.info(line)
+
+        return response
     
     return app
 
 if __name__ == '__main__': 
     app = create_app()
-    app.logger.debug("Running app in debug mode")
     app.run()
 
--- lightbulb/main/views.py
+++ lightbulb/main/views.py
@@ -8,34 +8,24 @@ main_blueprint = Blueprint('main', __name__, url_prefix='/', template_folder='te
 
 @main_blueprint.route('/', methods=['GET'])
 def index():
-    current_app.logger.debug("main.index()")
-    current_app.logger.debug(str(request))
     return render_template('main/index.html', meta_description=meta_description['index'], meta_keywords=meta_keywords['index'])
 
 @main_blueprint.route('/about', methods=['GET'])
 def about():
-    current_app.logger.debug("main.about()")
-    current_app.logger.debug(str(request))
     return render_template('main/about.html', meta_description=meta_description['about'], meta_keywords=meta_keywords['about'])
 
 @main_blueprint.route('/services/', methods=['GET'])
 @main_blueprint.route('/services', methods=['GET'])
 def services():
-    current_app.logger.debug("main.services()")
-    current_app.logger.debug(str(request))
     return render_template('main/services.html', meta_description=meta_description['services'], meta_keywords=meta_keywords['services'])
 
 @main_blueprint.route('/terminology/', methods=['GET'])
 @main_blueprint.route('/terminology', methods=['GET'])
 def terminology():
-    current_app.logger.debug("main.terminology()")
-    current_app.logger.debug(str(request))
     return render_template('main/terminology.html', meta_description=meta_description['terminology'], meta_keywords=meta_keywords['terminology'])
 
 @main_blueprint.route('/favicon.ico')
 @main_blueprint.route('/robots.txt')
 @main_blueprint.route('/sitemap.xml')
 def static_from_root():
-    current_app.logger.debug("main.static_from_root(): " + str(request))
-    current_app.logger.debug(f"main.static_from_root(): root_path = {current_app.root_path} request.path[1:] = {request.path[1:]}")
     return send_from_directory(current_app.root_path, request.path[1:])
